38af82260e8185e1871448e3b44cadca18208240
fr-javohir <frjavohir@gmail.com>
2023-02-11 11:19:46 +0500

    Now it is possible to add todos on html template

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 
 def home(request):
+    if request.method == 'POST':
+        sarlavha = request.POST.get('s')
+        vaqt = request.POST.get('v')
+        batafsil = request.POST.get('b')
+        holat = request.POST.get("st")
+        Todo.objects.create(sarlavha=sarlavha, vaqt=vaqt,
+                            batafsil=batafsil, holat=holat)
+
+        return redirect('/')
     data = {
         "messages": Todo.objects.all()
     }
     return render(request, 'todo.html', data)
-# Create your views here.
