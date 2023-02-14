ecd252842c409c0532a97b96558c182de4bbe80c
fr-javohir <frjavohir@gmail.com>
2023-02-14 10:19:42 +0500

    Now it is possible to login and logout

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,16 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Todo(models.Model):
     status = [
-        ("bajarildi","bajarildi"),
-        ("bajarilmoqda","bajarilmoqda"),
-        ("bajarilmadi","bajarilmadi"),
-        
-    ]     
+        ("bajarildi", "bajarildi"),
+        ("bajarilmoqda", "bajarilmoqda"),
+        ("bajarilmadi", "bajarilmadi"),
+
+    ]
     sarlavha = models.CharField(max_length=70)
     vaqt = models.DateField()
     batafsil = models.TextField()
-    holat = models.CharField(max_length=20,choices=status)
+    holat = models.CharField(max_length=20, choices=status)
+    foydalanuvchi = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
--- asosiy/views.py
+++ asosiy/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 
 
 def home(request):
-    if request.method == 'POST':
-        sarlavha = request.POST.get('s')
-        vaqt = request.POST.get('v')
-        batafsil = request.POST.get('b')
-        holat = request.POST.get("st")
-        Todo.objects.create(sarlavha=sarlavha, vaqt=vaqt,
-                            batafsil=batafsil, holat=holat)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            sarlavha = request.POST.get('s')
+            vaqt = request.POST.get('v')
+            batafsil = request.POST.get('b')
+            holat = request.POST.get("st")
+            Todo.objects.create(sarlavha=sarlavha, vaqt=vaqt,
+                                batafsil=batafsil, holat=holat, foydalanuvchi=request.user)
 
-        return redirect('/')
-    data = {
-        "messages": Todo.objects.all()
-    }
-    return render(request, 'todo.html', data)
+            return redirect('/todo')
+        data = {
+            "messages": Todo.objects.filter(foydalanuvchi=request.user)
+        }
+        return render(request, 'todo.html', data)
+    return redirect('/')
+
+
+def loginview(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get("l"),
+                            password=request.POST.get("p"))
+        if user is None:
+            return redirect('/')
+        login(request, user)
+        return redirect('/todo/')
+    return render(request, 'login.html')
+
+
+def logoutview(request):
+    logout(request)
+    return redirect('/')
